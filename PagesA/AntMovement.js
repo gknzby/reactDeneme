a3aae9919e32346f2b6ded5f17ff8d7fbcacc540
gknzby <gokhan_ozbey@outlook.com>
2020-12-22 03:56:03 +0300

    unimportant changes

--- PagesA/AntMovement.js
+++ PagesA/AntMovement.js
@@ -2,7 +2,20 @@ import React, {Component} from 'react';
 import { View, Text, Image, ScrollView, TextInput, StyleSheet, StatusBar, Button} from 'react-native';
 import { } from 'react-native';
 
+class AntMovHeader extends Component
+{
 
+    render()
+    {
+        return(
+            <View style = {styles.header}>
+                <View style = {styles.headerTitle}>
+                    <Text>{this.props.title}</Text>
+                </View>
+            </View>
+        );
+    }
+}
 
 
 class AntMovement extends Component
@@ -12,9 +25,27 @@ class AntMovement extends Component
     render()
     {
         return(
-            <Text>Deeme</Text>
+            <View>
+                <AntMovHeader/>
+            </View>
         );
     }
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#dddddd',
+        borderBottomWidth: 2,
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    headerTitle: {
+        flex: 1,
+        alignItems: 'center',
+    },
 }
 
 export default AntMovement;
